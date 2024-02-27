56b409476010587bc74f47a54a5ed7119bd3c3ed
Alexander Gerhard <agerkhard@outlook.com>
2024-02-27 17:08:16 +0600

    Updated scritps to work with a tablespace

---
+++ tools/tablespace_list_datafiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sqlplus -S -L / as sysdba << EOF
+SET LINESIZE 200
+COLUMN file_name FORMAT A70
+
+SELECT file_id,
+       file_name,
+       ROUND(bytes/1024/1024/1024) AS size_gb,
+       ROUND(maxbytes/1024/1024/1024) AS max_size_gb,
+       autoextensible,
+       increment_by,
+       status
+FROM   dba_data_files
+ORDER BY file_name;
+quit
+EOF
